a430901414e7b052088d202b5f8b12969af24d61
Sojin Lee <sojinlee0z0@gmail.com>
2024-08-05 20:41:09 +0900

    feat: project setting

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,16 +1,24 @@
 import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
-import {schemaTypes} from './schemaTypes'
+import {schemaTypes} from './schemas'
+import {media} from 'sanity-plugin-media'
+import {markdownSchema} from 'sanity-plugin-markdown'
+// import {CustomMarkdownInput} from './custom_components/custom-markdown-input'
 
 export default defineConfig({
   name: 'default',
   title: 'portfolio',
 
-  projectId: 'oblkb0f0',
+  projectId: process.env.SANITY_STUDIO_PROJECT_ID!,
   dataset: 'production',
 
-  plugins: [structureTool(), visionTool()],
+  plugins: [
+    structureTool(),
+    visionTool(),
+    media(),
+    // markdownSchema({input: CustomMarkdownInput}),
+  ],
 
   schema: {
     types: schemaTypes,
