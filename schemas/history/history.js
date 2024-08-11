b7c6b66670b765b08a1ff866b7ecd9f3a4682189
Sojin Lee <sojinlee0z0@gmail.com>
2024-08-11 16:05:37 +0900

    Fix: 이력 미리보기 수정

--- schemas/history/history.js
+++ schemas/history/history.js
@@ -1,19 +1,20 @@
 import {defineType, defineField} from 'sanity'
 
+const YEARS = [
+  {title: '이전', value: 0},
+  {title: '2019', value: 2019},
+  {title: '2020', value: 2020},
+  {title: '2021', value: 2021},
+  {title: '2022', value: 2022},
+  {title: '2023', value: 2023},
+  {title: '2024', value: 2024},
+]
 const yearField = defineField({
   title: '년도',
   name: 'year',
   type: 'number',
   options: {
-    list: [
-      {title: '이전', value: 0},
-      {title: '2019', value: 2019},
-      {title: '2020', value: 2020},
-      {title: '2021', value: 2021},
-      {title: '2022', value: 2022},
-      {title: '2023', value: 2023},
-      {title: '2024', value: 2024},
-    ],
+    list: YEARS,
   },
   validation: (Rule) => Rule.required(),
 })
@@ -21,7 +22,13 @@ const yearField = defineField({
 const contentField = defineField({
   title: '내용',
   name: 'content',
-  type: 'string',
+  type: 'array',
+  description: 'list로 작성할 것',
+  of: [
+    {
+      type: 'block',
+    },
+  ],
   validation: (Rule) => Rule.required(),
 })
 
@@ -32,7 +39,14 @@ export default defineType({
   fields: [yearField, contentField],
   preview: {
     select: {
-      title: 'content',
+      year: 'year',
+    },
+    prepare: ({year}) => {
+      const yearName =
+        year && YEARS.flatMap((option) => (option.value === year ? [option.title] : []))
+      return {
+        title: `${yearName}년의 이력`,
+      }
     },
   },
 })
