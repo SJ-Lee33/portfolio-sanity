ba124575b5d4fee216628af55b49410239d2d66f
Sojin Lee <sojinlee0z0@gmail.com>
2025-09-05 19:08:12 +0900

    fix: 이력 2025년 추가

--- schemas/history/history.js
+++ schemas/history/history.js
@@ -2,12 +2,12 @@ import {defineType, defineField} from 'sanity'
 
 const YEARS = [
   {title: '이전', value: 0},
-  {title: '2019', value: 2019},
   {title: '2020', value: 2020},
   {title: '2021', value: 2021},
   {title: '2022', value: 2022},
   {title: '2023', value: 2023},
   {title: '2024', value: 2024},
+  {title: '2025', value: 2025}
 ]
 
 const yearField = defineField({
