98722a2d645897fbd7950b77723417b8af9a11e7
Sojin Lee <sojinlee0z0@gmail.com>
2025-09-10 14:26:53 +0900

    refactoring: env

---
+++ env.ts
@@ -0,0 +1,3 @@
+export const apiVersion = process.env.SANITY_API_VERSION || '2025-09-10'
+export const projectId = process.env.SANITY_STUDIO_PROJECT_ID
+export const dataset = process.env.SANITY_DATASET || 'production'
--- sanity.config.ts
+++ sanity.config.ts
@@ -4,6 +4,7 @@ import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemas'
 import {media} from 'sanity-plugin-media'
 import {codeInput} from '@sanity/code-input'
+import {projectId, dataset} from './env'
 
 export default defineConfig({
   name: 'default',
@@ -11,6 +12,8 @@ export default defineConfig({
 
   projectId: process.env.SANITY_STUDIO_PROJECT_ID!,
   dataset: 'production',
+  projectId: projectId!,
+  dataset: dataset!,
 
   plugins: [structureTool(), visionTool(), media(), codeInput()],
 
